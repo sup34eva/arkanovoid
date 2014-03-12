17d3d3b111c8a44d68f155be97b7a7e25c77a09e
leops <leopaul3@gmail.com>
2014-03-12 13:26:25 +0100

    Ajout de stylus

--- grunt/stylus.js
+++ grunt/stylus.js
@@ -1,11 +1,10 @@
 module.exports = {
-    compile: {
-        options: {
-            urlfunc: 'embedurl'
-        },
-        files: {
-            'dist/css/style.css': ['stylus/**/*.styl', 'stylus/**/*.stylus']
-        }
-    }
+	compile: {
+		options: {
+			urlfunc: 'embedurl'
+		},
+		files: {
+			'dist/css/style.css': ['styles/**/*.styl', 'styles/**/*.stylus']
+		}
+	}
 }
-
