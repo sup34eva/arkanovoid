49765c1cd67d5c57d815038121bf42f2cd272f3c
leops <leopaul3@gmail.com>
2014-03-12 17:01:59 +0100

    Ajout d'une commande de test

---
+++ grunt/coffeelint.js
@@ -0,0 +1,16 @@
+module.exports = {
+	app: ['coffee/*.coffee'],
+	options: {
+		'no_tabs': {
+			'level': 'ignore'
+		},
+		'max_line_length': {
+			'level': 'warn',
+			'value': 130
+		},
+		indentation: {
+			'level': 'warn',
+			'value': 1
+		}
+	}
+};
