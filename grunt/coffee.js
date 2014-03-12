75e2b532b29abfc9cef7fe20131586f7a00496b4
leops <leopaul3@gmail.com>
2014-03-12 13:22:20 +0100

    Les coffescript sont compilés dans un ordre determinés et le fichier pré-minifié est placé dans un dossier temporaire

--- grunt/coffee.js
+++ grunt/coffee.js
@@ -1,9 +1,26 @@
 module.exports = {
-    options: {
-    },
-    dist: {
-        src: ['coffee/**/*.coffee'],
-        dest: 'dist/<%= package.name %>.js'
-    }
+	options: {
+		join: true,
+		sourceMap: true
+	},
+	dist: {
+		src: [
+			'coffee/util.coffee',
+			'coffee/ball.coffee',
+			'coffee/paddle.coffee',
+			'coffee/brick.coffee',
+			'coffee/init.coffee',
+			'coffee/draw.coffee',
+			'coffee/server.coffee',
+			'coffee/events.coffee'
+		],
+		dest: '.tmp/<%= package.name %>.js'
+	},
+	worker: {
+		src: [
+			'coffee/util.coffee',
+			'coffee/worker.coffee'
+		],
+		dest: 'dist/js/worker.js'
+	}
 };
-
--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,10 +1,16 @@
 module.exports = {
-  options: {
-    banner: '/*! <%= package.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-  },
-  dist: {
-    files: {
-      'dist/<%= package.name %>.min.js': 'dist/<%= package.name %>.js'
-    }
-  }
+	options: {
+		banner: '/*! <%= package.title || package.name %> - v<%= package.version %> - ' +
+			'<%= grunt.template.today("dd/mm/yyyy") %> - ' +
+			'<%= package.homepage %>\n' +
+			'* Copyright (c) <%= package.author %> <%= grunt.template.today("yyyy") %>;' +
+			' Licensed <%= package.license %> */\n',
+		beautify: true,
+		mangle: false
+	},
+	dist: {
+		files: {
+			'dist/js/<%= package.name %>.min.js': '.tmp/<%= package.name %>.js'
+		}
+	}
 };
