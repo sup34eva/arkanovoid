e99451e6a9205ab2782663f621ff99989a6e8198
leops <leopaul3@gmail.com>
2014-03-12 13:24:07 +0100

    Enregistrement des logs d'express dans un fichier plutot que dans la console

--- grunt/express.js
+++ grunt/express.js
@@ -1,11 +1,14 @@
 module.exports = {
-    options: {
-    },
-    server: {
-        options: {
-            script: 'main.js',
-            port: 3000,
-            node_env: 'development'
-        }
-    }
+	options: {},
+	server: {
+		options: {
+			script: 'main.js',
+			port: 3000,
+			node_env: 'development',
+			logs: {
+				out: 'logs/out.log',
+				err: 'logs/err.log'
+			}
+		}
+	}
 };
