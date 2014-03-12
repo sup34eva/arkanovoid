72c1f94e22889b50acca7b03d521c82e5ca0eeb2
leops <leopaul3@gmail.com>
2014-03-12 13:25:33 +0100

    Ajout de livereload

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,24 +1,26 @@
 module.exports = {
-    express: {
-        files:  ['main.js', 'routes.json'],
-        tasks:  ['express:server', 'notify:server'],
-        options: {
-            spawn: false
-        }
-    },
-    coffee: {
-        files:  ['coffee/**/*.coffee'],
-        tasks:  ['coffeescript', 'notify:coffee'],
-        options: {
-            spawn: false
-        }
-    },
-    style: {
-        files:  ['stylus/**/*.styl', 'stylus/**/*.stylus'],
-        tasks:  ['stylus', 'notify:stylus'],
-        options: {
-            spawn: false
-        }
-    }
+	express: {
+		files: ['main.js', 'routes.json'],
+		tasks: ['express:server', 'notify:server'],
+		options: {
+			spawn: false,
+			livereload: true
+		}
+	},
+	coffee: {
+		files: ['coffee/**/*.coffee'],
+		tasks: ['coffeescript', 'notify:coffee'],
+		options: {
+			spawn: false,
+			livereload: true
+		}
+	},
+	style: {
+		files: ['stylus/**/*.styl', 'stylus/**/*.stylus'],
+		tasks: ['stylus', 'notify:stylus'],
+		options: {
+			spawn: false,
+			livereload: true
+		}
+	}
 };
-
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var express = require('express'),
 	app = express(),
-	routes = require('./routes.json');
+	routes = require('./routes.json'),
+	port = process.env.port || 3000;
 
 app.use(express.compress());
 app.use(express.logger('tiny'));
-app.use(require('connect-livereload')({
-	port: 80
-}));
+app.use(require('connect-livereload')());
 app.use(app.router);
 app.use(express.static('dist'));
+app.use('/img', express.static('img'));
 app.use('/server', express.static('server'));
 app.use('/bower', express.static('bower_components'));
 app.use('/lvledit', express.static('lvledit'));
@@ -19,17 +19,10 @@ function handler(req, res) {
 	res.render(routes[req.route.path].template, routes[req.route.path].args);
 }
 
-for(var i in routes)
+for (var i in routes)
 	app.get(i, handler);
 
-app.get('/livereload.js', function(req, res) {
-	require('http').get('http://localhost:35729/livereload.js').on('response', function (pres) {
-		pres.pipe(res);
-		res.writeHead(pres.statusCode, pres.headers);
-	});
-});
-
-function kiddies (req, res) {
+function kiddies(req, res) {
 	res.send(418, "I'm a teapot");
 }
 
@@ -40,7 +33,6 @@ app.get('/pma/scripts/setup.php', kiddies);
 app.get('/myadmin/scripts/setup.php', kiddies);
 app.get('/MyAdmin/scripts/setup.php', kiddies);
 
-var port = process.env.port || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
 	console.log('Listening on port ' + port);
 });
