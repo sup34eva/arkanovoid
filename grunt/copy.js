3f1283a806642185888c42f26445480cf1588716
leops <leopaul3@gmail.com>
2014-03-13 12:41:38 +0100

    Suppression d'imagemin

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,9 +1,18 @@
 module.exports = {
-	main: {
+	server: {
 		files: [
 			{
 				expand: true,
 				src: ['server/server.pexe', 'server/server.nmf'],
+				dest: 'dist/'
+			}
+		]
+	},
+	img: {
+		files: [
+			{
+				expand: true,
+				src: ['img/*.{png,jpg,gif}'],
 				dest: 'dist/'
 			}
 		]
