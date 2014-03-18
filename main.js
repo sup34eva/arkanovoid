dea41d486ce4e91c856c678417be0167fd699dbf
leops <leopaul3@gmail.com>
2014-03-18 10:56:26 +0100

    Ajout de stats au serveur

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ app.use(express.static('dist'));
 app.use('/bootstrap', express.static('bower_components/bootstrap/dist'));
 app.use('/jquery', express.static('bower_components/jquery/dist'));
 app.use('/two', express.static('bower_components/two/build'));
+app.use('/stats', express.static('bower_components/stats.js/build'));
 app.use('/lvledit', express.static('lvledit'));
 app.use(errors.send404);
 app.use(errors.log);
